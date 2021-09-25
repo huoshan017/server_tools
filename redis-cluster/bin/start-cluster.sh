9b6bc3ce957fd21218c469a3360ae6ed523f0a05
huoshan017 <huoshan017@hotmail.com>
2021-09-25 22:28:02 +0800

    update

---
+++ redis-cluster/bin/start-cluster.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo ./redis-server node-6379.conf --protected-mode no &
+sudo ./redis-server node-6380.conf --protected-mode no &
+sudo ./redis-server node-6381.conf --protected-mode no &
+sudo ./redis-server node-6382.conf --protected-mode no &
+sudo ./redis-server node-6383.conf --protected-mode no &
+sudo ./redis-server node-6384.conf --protected-mode no &
---
+++ redis-cluster/bin/stop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo ./redis-cli -p 6379 shutdown
+sudo ./redis-cli -p 6380 shutdown
+sudo ./redis-cli -p 6381 shutdown
+sudo ./redis-cli -p 6382 shutdown
+sudo ./redis-cli -p 6383 shutdown
+sudo ./redis-cli -p 6384 shutdown
